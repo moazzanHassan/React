e3345ade995655df3572fedf8ce0aa2d2e072b95
Moazzan Hassan <moazzanhassan907@gmail.com>
2025-02-20 09:43:13 +0500

    file making

--- employeManagementSystem/src/App.jsx
+++ employeManagementSystem/src/App.jsx
@@ -1,15 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import Login from './components/Auth/Login'
 function App() {
   
   return (
     <>
-      <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+    <Login />      
     </>
   )
 }
---
+++ employeManagementSystem/src/components/Auth/Login.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div className=' flex justify-center items-center'>
+        <div className='w-1/2 h-screen border-2 border-amber-200'>
+          <form >
+
+          </form>
+        </div>
+    </div>
+  )
+}
+
+export default Login
