7751d0028268d97245693c73f90781c5eadf1c12
Moazzan Hassan <moazzanhassan907@gmail.com>
2025-02-21 10:13:00 +0500

    styling login page

--- employeManagementSystem/src/components/Auth/Login.jsx
+++ employeManagementSystem/src/components/Auth/Login.jsx
@@ -1,15 +1,29 @@
-import React from 'react'
+import React from 'react';
 
 const Login = () => {
   return (
-    <div className=' flex justify-center items-center'>
-        <div className='w-1/2 h-screen border-2 border-amber-200'>
-          <form >
-
-          </form>
-        </div>
+    <div className="flex h-screen w-screen justify-center items-center bg-gray-900">
+      <div className="border-2 h-1/2 w-1/3 border-emerald-600 rounded-xl "style={{padding:"20px"}}>
+        <form className="flex flex-col justify-center items-center h-full space-y-4">
+          <input
+          style={{ padding: '10px' }}
+            className="border-2 bg-transparent border-emerald-600 outline-none text-white placeholder:text-gray-400 text-xl rounded-full px-4 py-2 w-full"
+            type="email"
+            placeholder="Enter your email"
+          />
+          <input
+          style={{ padding: '10px', marginTop:"10px" }}
+            className="border-2 bg-transparent border-emerald-600 outline-none text-white placeholder:text-gray-400 text-xl rounded-full px-4 py-2 w-full"
+            type="password"
+            placeholder="Enter your password"
+          />
+          <button  style={{ paddingLeft: '20px', paddingRight: '20px', paddingTop: '10px' ,paddingBottom: '10px',marginTop:'10PX' }} className="border-2 bg-emerald-600 outline-none text-white text-xl rounded-full px-10 py-2 hover:bg-emerald-700 transition-all">
+            Login
+          </button>
+        </form>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
