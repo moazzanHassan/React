850c738234984cbeae03e55ad8f41579da054216
Moazzan Hassan <moazzanhassan907@gmail.com>
2025-02-25 11:52:47 +0500

    styling

--- employeManagementSystem/src/components/Dashboard/EmployeeDashboard.jsx
+++ employeManagementSystem/src/components/Dashboard/EmployeeDashboard.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from '../Other/Header'
+import TaskListNumbers from '../Other/TaskListNumbers'
 
 const EmployeeDashboard = () => {
   return (
-    <div>
+    <div style={{padding:"20px"}} className='h-screen bg-[#1C1C1C]'>
         <Header />
+        <TaskListNumbers />
     </div>
   )
 }
--- employeManagementSystem/src/components/Other/Header.jsx
+++ employeManagementSystem/src/components/Other/Header.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 const Header = () => {
   return (
     <div>
-        <div className='flex justify-between items-end'>
-            <h1>Helllo Moazzan👋</h1>
-            <button>Log in </button>
+        <div className='flex justify-between items-end text-white '>
+            <h1 className='text-2xl px-56'>Helllo <span className='text-3xl'>Moazzan👋</span></h1>
+            <button style={{padding:"10px 20px"}} className='bg-red-500 rounded-xl'>Log in </button>
         </div>
     </div>
   )
